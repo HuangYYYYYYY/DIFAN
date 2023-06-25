510d470da6dd9b1e155b3104236e204edaed19c1
Grant.H <109274911+HuangYYYYYYY@users.noreply.github.com>
2023-06-25 14:50:17 +0800

    Update DIAC.py

--- models/DIAC.py
+++ models/DIAC.py
@@ -27,23 +27,18 @@ def SAC(feat_in, kernel1, kernel2, ksize):
     N, kernels, H, W = kernel1.size()
     pad = (ksize - 1) // 2
 
-    sfLaKD = DLKD_block(in_channels=channels,out_channel=channels)
-
     feat_in = Func.pad(feat_in, (0, 0, pad, pad), mode="replicate")
     feat_in = feat_in.unfold(2, ksize, 1)
     feat_in = feat_in.permute(0, 2, 3, 1, 4).view(N, H, W, channels, -1)
 
-    feat_in = sfLaKD(feat_in)
     kernel1 = kernel1.permute(0, 2, 3, 1).view(N, H, W, channels, ksize)
     feat_in = torch.sum(torch.mul(feat_in, kernel1), -1)
     feat_in = feat_in.permute(0, 3, 1, 2)
-
 
     feat_in = Func.pad(feat_in, (pad, pad, 0, 0), mode="replicate")
     feat_in = feat_in.unfold(3, ksize, 1)
     feat_in = feat_in.permute(0, 2, 3, 1, 4).view(N, H, W, channels, -1)
 
-    feat_in = sfLaKD(feat_in)
     kernel2 = kernel2.permute(0, 2, 3, 1).view(N, H, W, channels, ksize)
     feat_in = torch.sum(torch.mul(feat_in, kernel2), -1)
     feat_out = feat_in.permute(0, 3, 1, 2)
